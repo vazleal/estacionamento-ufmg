297413e977da4140424e949ca92755201d2a91f2
VKenji-pa <84997899+VKenji-pa@users.noreply.github.com>
2025-06-02 15:24:29 -0300

    Update main.tsx

--- frontend/react-ts/src/main.tsx
+++ frontend/react-ts/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import PainelEstacionamento from './App'
-import LoginPage from './pages/LoginPage/LoginPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
